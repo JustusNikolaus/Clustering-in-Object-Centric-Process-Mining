4d51f4a1e86956aad338491a32f845a04e121070
Justus Nikolaus <justus.nikolaus@googlemail.com>
2021-11-05 09:47:37 +0100

    No changes

--- ciocel/settings.py
+++ ciocel/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -120,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+print(STATIC_ROOT)
 # Files
 # https://docs.djangoproject.com/en/3.2/topics/files/
 MEDIA_URL = '/media/'
