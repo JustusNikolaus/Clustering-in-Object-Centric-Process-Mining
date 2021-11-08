364930cbd217968ee805a8e363af12cb1ffc994f
simonmichau <57032947+simonmichau@users.noreply.github.com>
2021-11-08 17:40:49 +0100

    Object selection dropdown menu now displays object types from selected file; Cleaned up some print()'s from debugging

--- drawpage/views.py
+++ drawpage/views.py
@@ -2,15 +2,26 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 import os
+import drawpage.readocel
 
 # Create your views here.
 def drawpage_view(request):
+    # Standard Handler
     log_list = []
     ext = ('.csv','.jsonocel')
     for files in os.listdir("media/"):
         if files.endswith(ext):
             log_list.append(files)
-            print(log_list)
         else:
             continue
     return render(request,'drawpage/drawpage.html', {'log_list':log_list})
+
+def fileselect_view(request):
+    # File Select Handler
+    object_type_list = []
+    if request.method == 'POST':
+        selected_file = request.POST['file_select']
+        ocel_object_dict_list = drawpage.readocel.get_object_information('media/' + selected_file)
+        for i in range(len(ocel_object_dict_list)):
+            object_type_list.append(ocel_object_dict_list[i].get('object_type'))
+    return render(request,'drawpage/drawpage.html', {'object_type_list':object_type_list})
