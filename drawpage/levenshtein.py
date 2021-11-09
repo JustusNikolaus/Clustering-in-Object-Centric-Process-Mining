a00096643fd640921d3013907e90f8605bb853bc
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2021-11-09 21:09:06 +0100

    Update levenshtein.py to include activity labelling
    Includes activity/object labelling for control flows, still requires the concatenation.

--- drawpage/levenshtein.py
+++ drawpage/levenshtein.py
@@ -1,8 +1,30 @@
 from Levenshtein import distance as levdistance
+from sklearn.utils.multiclass import unique_labels
+import string
 
-# Return: alphabet labels for each activity within a control flow
-# Input: list = list of activities within a control flow
-#def label_activities(list: list) -> list:
+# Return: alphabet labels for each activity/object within a control flow
+# Input: list = list of activities/objects within a control flow
+def label_activities(list: list) -> list:
+    #print(list)
+    activity_labels = [] # array for the unique activity/object labels
+    unique_activities = unique_labels(list) # sorting throught the input and using only unique strings
+    #print(unique_activities)
+    labels = string.ascii_letters + string.digits + string.punctuation # alphabet, digit and, punctuation labels
+    label_counter = 0 # counter for traversing through the labels
+    for items in unique_activities: 
+        activity_labels.append(labels[label_counter])
+        label_counter += 1
+    print(activity_labels)
+    labels_map = zip(unique_activities, activity_labels) # to see which labels belong to which activities
+    print(set(labels_map))
+
+# Labelling test
+#activities = ['eating', 'drinking', 'sleeping', 'drinking', 'drinking and sleeping', 'showering', 'eating']
+#label_activities(activities)
+
+# Return: concatenated control flow using the labels from label_activities
+# Input: list = list of control flows 
+#def concatenate_labels(list: list) -> list:
 
 # Return: distances[[]] between all pairs of control flows (represented by strings) in a list of control flows
 # Input: list = list of control flows
