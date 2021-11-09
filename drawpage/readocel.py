36c780ca069b4043e251a8ab73d552bb9d489d9b
simonmichau <57032947+simonmichau@users.noreply.github.com>
2021-11-09 16:10:38 +0100

    Updated the drawpage to work with sessions

--- drawpage/readocel.py
+++ drawpage/readocel.py
@@ -4,23 +4,26 @@ from pm4pymdl.objects.ocel.importer import importer as ocel_importer
 # Input: path_to_file = Path to selected OCEL file
 def get_object_information(path_to_file: str) -> dict:
     object_information = []
-    event_df, object_df = ocel_importer.apply(path_to_file)
-    # Stores unique values of column 'object_type' into array object_types
-    object_types = object_df.object_type.unique()
-    # Loop over every object_type
-    for obj in object_types:
-        # Give default control flow attribute
-        attributes = ["control flow"]
-        rows_of_obj = object_df.loc[object_df['object_type']==obj]
-        # Get all columns that are not null for select object_type
-        for (column_name, series) in object_df.iteritems():
-            if column_name != "object_id" and column_name != "object_type":
-                if len(rows_of_obj.loc[rows_of_obj[column_name].notnull()]) > 0:
-                    attributes.append(column_name)
-        # Create a dict that stores the information for given object_type
-        dict = {
-            "object_type": obj,
-            "attributes": attributes
-        }
-        object_information.append(dict)    
+    try:
+        event_df, object_df = ocel_importer.apply(path_to_file)
+        # Stores unique values of column 'object_type' into array object_types
+        object_types = object_df.object_type.unique()
+        # Loop over every object_type
+        for obj in object_types:
+            # Give default control flow attribute
+            attributes = ["control flow"]
+            rows_of_obj = object_df.loc[object_df['object_type']==obj]
+            # Get all columns that are not null for select object_type
+            for (column_name, series) in object_df.iteritems():
+                if column_name != "object_id" and column_name != "object_type":
+                    if len(rows_of_obj.loc[rows_of_obj[column_name].notnull()]) > 0:
+                        attributes.append(column_name)
+            # Create a dict that stores the information for given object_type
+            dict = {
+                "object_type": obj,
+                "attributes": attributes
+            }
+            object_information.append(dict)   
+    except:
+        print("File import failed.") 
     return object_information
--- drawpage/views.py
+++ drawpage/views.py
@@ -1,27 +1,32 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
 import os
 import drawpage.readocel
 
+
 # Create your views here.
 def drawpage_view(request):
-    # Standard Handler
     log_list = []
+    object_type_list = []
+
+    request.session['fav_color'] = 'blue'
+    #File Select
+    if request.method == 'POST':
+        selected_file = request.POST['file_select']
+
+        ocel_object_dict_list = drawpage.readocel.get_object_information('media/' + selected_file)
+
+        for i in range(len(ocel_object_dict_list)):
+            object_type_list.append(ocel_object_dict_list[i].get('object_type'))
+        
+        request.session['filecookie'] = selected_file
+        log_list.insert(0, request.session['filecookie'])
+    else:
+        log_list.insert(0, "Choose OCEL")
+
     ext = ('.csv','.jsonocel')
-    for files in os.listdir("media/"):
+    for files in os.listdir('media/'):
         if files.endswith(ext):
             log_list.append(files)
         else:
             continue
-    return render(request,'drawpage/drawpage.html', {'log_list':log_list})
-
-def fileselect_view(request):
-    # File Select Handler
-    object_type_list = []
-    if request.method == 'POST':
-        selected_file = request.POST['file_select']
-        ocel_object_dict_list = drawpage.readocel.get_object_information('media/' + selected_file)
-        for i in range(len(ocel_object_dict_list)):
-            object_type_list.append(ocel_object_dict_list[i].get('object_type'))
-    return render(request,'drawpage/drawpage.html', {'object_type_list':object_type_list})
+    return render(request, 'drawpage/drawpage.html', {'log_list':log_list, 'object_type_list':object_type_list})
