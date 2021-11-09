de9e0cdc569bdd06c60dcc6837f1bdac9c022148
simonmichau <57032947+simonmichau@users.noreply.github.com>
2021-11-09 19:24:58 +0100

    Added error message to Importpage

--- importpage/views.py
+++ importpage/views.py
@@ -9,14 +9,17 @@ def importpage_view(request):
 def upload_log(request):
     response_label = []
     if request.method == 'POST':
-        uploaded_file = request.FILES['log']
-        fs = FileSystemStorage()
+        try:
+            uploaded_file = request.FILES['log']
+            fs = FileSystemStorage()
 
-        if uploaded_file.name.endswith('.jsonocel'):
-            fs.save(uploaded_file.name, uploaded_file)
-            response_label = ['success','File upload successful']
-        else:
-            response_label = ['danger','File format must be .jsonocel']
+            if uploaded_file.name.endswith('.jsonocel'):
+                fs.save(uploaded_file.name, uploaded_file)
+                response_label = ['success','File upload successful']
+            else:
+                response_label = ['danger','File format must be .jsonocel']
+        except:
+            response_label = ['danger','Please select a file first']
     return render(request, 'importpage/importpage.html', {'response_label':response_label})
 
 
