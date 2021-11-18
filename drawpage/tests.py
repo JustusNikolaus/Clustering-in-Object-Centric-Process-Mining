f2453a3356188eba5e0ffd71b7623d8e6e4a2f35
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2021-11-18 23:06:54 +0100

    Update tests.py
    Small update to the matrix generation for Euclidean distance testing (more random).

--- drawpage/tests.py
+++ drawpage/tests.py
@@ -6,19 +6,23 @@ import numpy as np
 
 # Create your tests here.
 
-
+# List of control flows
 lev_activities = [['eat', 'sleep', 'rave', 'repeat'],
                    ['eat', 'sleep', 'rave', 'Not repeat'],
                    ['eat', 'sleep', 'Not rave', 'Not repeat'],
                    ['eat', 'Not sleep', 'Not rave', 'Not repeat'],
                    ['Not eat', 'Not sleep', 'Not rave', 'Not repeat']]
 
+# List of objects
 bool_activities = ['Not eat', 'Not sleep', 'Not rave', 'Not repeat', 'eat', 
                     'Not sleep', 'Not rave', 'Not repeat', 'Not eat', 'Not sleep', 
                     'Not rave', 'Not repeat', 'eat', 'Not sleep', 'Not rave', 
                     'Not repeat']
 
-euc_activities = np.random.randint(20, size=(3,35))
+# List of datapoints
+matrix_size_x = np.random.randint(low = 2, high = 20)
+matrix_size_y = np.random.randint(low = 2, high = 500)
+euc_activities = np.random.randint(20, size=(matrix_size_x, matrix_size_y))
 
 
 test_lev = LevenshteinDistance(lev_activities).get_levenshtein_distances(lev_activities)
@@ -26,7 +30,7 @@ print("Calculated Levenshtein distances:\n", test_lev, "\n-----------")
 test_bool = BooleanDistance(bool_activities).get_boolean_distances(bool_activities)
 print("Calculated Boolean distances:\n", test_bool, "\n-----------")
 test_euc = EuclideanDistance(euc_activities).get_euclidean_distances(euc_activities)
-print("Calculated Euclidean distances:\n", test_euc, "\n-----------")
+print("Calculated Euclidean distances (matrix size {} by {}):\n".format(matrix_size_x, matrix_size_y), test_euc, "\n-----------")
 
 # Test k_medoids
 
