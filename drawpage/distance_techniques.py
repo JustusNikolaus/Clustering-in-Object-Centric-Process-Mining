8e13bde48ec39e76658eebc7e412029b5b07228a
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2021-11-18 22:55:45 +0100

    Update distance_techniques.py
    Changes to the look of the code, checked against PEP8 requirements.

--- drawpage/distance_techniques.py
+++ drawpage/distance_techniques.py
@@ -7,7 +7,9 @@ import string
 # For arrays and the removal of diagonals
 import numpy as np
 
-from sklearn.neighbors import DistanceMetric # For the calculation of the Euclidean distance
+# For the calculation of the Euclidean distance
+from sklearn.neighbors import DistanceMetric
+
 
 class LevenshteinDistance:
 
@@ -20,24 +22,30 @@ class LevenshteinDistance:
     def map_labels_to_activities(self, labels: list, activities: list) -> list:
         map = {}
         if len(labels) != len(activities):
-            print(0) # needs to return an error if they are not equal
-        for given_label, labelled_activity in zip(labels, activities): # for each given label
-            if (given_label not in map.keys()): # if the key is not already in the map
-                map[given_label] = labelled_activity # create key and add the corresponding activity
+            print(0)  # needs to return an error if they are not equal
+        # for each given label
+        for given_label, labelled_activity in zip(labels, activities):
+            # if the key is not already in the map
+            if (given_label not in map.keys()):
+                # create key and add the corresponding activity
+                map[given_label] = labelled_activity
             else:
-                print(0) # needs to return an error if they are not unique
+                print(0)  # needs to return an error if they are not unique
         return map
 
     # Return: alphabet labels for each activity/object within a control flow
     # Input: list = list of activities/objects within a control flow
     def label_activities(self, list: list) -> list:
         #print(list)
-        activity_labels = [] # array for the unique activity/object labels
-        unique_activities = unique_strings(list) # sorting through the input and using only unique strings
-        labels_map = {} # dictionary to map out each activity by a corresponding label
+        activity_labels = []  # array for the unique activity/object labels
+        # sorting through the input and using only unique strings
+        unique_activities = unique_strings(list)  
+        # dictionary to map out each activity by a corresponding label
+        labels_map = {}  
         #print(unique_activities)
-        labels = string.ascii_letters + string.digits + string.punctuation # alphabet, digit and, punctuation labels
-        label_counter = 0 # counter for traversing through the labels
+        # alphabet, digit and, punctuation labels
+        labels = string.ascii_letters + string.digits + string.punctuation  
+        label_counter = 0  # counter for traversing through the labels
         for items in unique_activities:
             activity_labels.append(labels[label_counter])
             label_counter += 1
@@ -46,49 +54,65 @@ class LevenshteinDistance:
     # Return: concatenated control flow using the labels from label_activities
     # Input: list = list of control flows
     def label_control_flows(self, list: list) -> list:
-        new_list = [] # new list to list out all items within the control flows list
-        labelled_control_flows = [] # list of the concatenated strings for the control flows
-        list_map = {} # new dictionary to map out the labels to the activities
-        for sublist in list: # going through the sublists to add them to the new list
+        # new list to list out all items within the control flows list
+        new_list = []
+        # list of the concatenated strings for the control flows
+        labelled_control_flows = []
+        list_map = {}  # new dictionary to map out the labels to the activities
+        # going through the sublists to add them to the new list
+        for sublist in list:  
             new_list += sublist
         #print(new_list)
-        given_labels = self.label_activities(new_list) # giving unique labels to all items within the passed control flows list
+        # giving unique labels to all items within the passed control flows list
+        given_labels = self.label_activities(new_list)  
         #print(given_labels) 
-        list_map = self.map_labels_to_activities(given_labels, unique_strings(new_list)) # creating a dict of the labels and the unique list items
+        # creating a dict of the labels and the unique list items
+        list_map = self.map_labels_to_activities(given_labels, unique_strings(new_list))  
         #print(list_map)
         # creating the concatenation of characters for each sublist
-        list_map_rev = {v: k for k, v in list_map.items()} # reverse the dictionary so that the characters are the values instead of keys
-        for sublist in list: # going through the control flows within the original list
-            control_flow = "" # string for the control flow labels
+        # reverse the dictionary so that the characters are the values instead of keys
+        list_map_rev = {v: k for k, v in list_map.items()}  
+        for sublist in list:  # going through the control flows within the original list
+            control_flow = ""  # string for the control flow labels
             for given_activity in sublist:
                 if given_activity in list_map_rev.keys():
-                    control_flow += list_map_rev[given_activity] # adds the character label to the control flow label
+                    # adds the character label to the control flow label
+                    control_flow += list_map_rev[given_activity] 
                 else:
-                    print(0) # should give an error if it is not a key
-            labelled_control_flows.append(control_flow) # adds the labelled control flow to the list
+                    print(0)  # should give an error if it is not a key
+            # adds the labelled control flow to the list
+            labelled_control_flows.append(control_flow)
         return labelled_control_flows
 
-    # Return: distances[[]] between all pairs of control flows (represented by strings) in a list of control flows
+    # Return: distances[[]] between all pairs of control flows 
+    # (represented by strings) in a list of control flows
     # Input: list = list of control flows
     def get_levenshtein_distances(self, list: list) -> list:
         labelled_list = self.label_control_flows(list)
         distances = []
         # First pointer, outer loop: traverse each word in the list once
-        # The number of inner lists within distances = the number of strings in the first iteration
+        # The number of inner lists within distances is 
+        # equal to the number of strings in the first iteration
         for firstStrings in labelled_list:
             sub_distances = []
-            # Second pointer, inner loop: traverse each word for each time it is traversed in the outer loop
-            # The number of output integers = the number of strings in the second iteration
+            # Second pointer, inner loop: traverse each word for 
+            # each time it is traversed in the outer loop
+            # The number of output integers is 
+            # equal to the number of strings in the second iteration
             for secondStrings in labelled_list:
                 sub_distances.append(levdistance(firstStrings, secondStrings))
             distances.append(sub_distances)
         return distances
 
 # Levenshtein test
-#con_activities = [['eating', 'drinking', 'sleeping', 'drinking', 'drinking and sleeping', 'showering', 'eating'],
-#                 ['drinking', 'eating', 'drinking and eating', 'sleeping', 'doing nothing', 'eating', 'drinking and sleeping'],
-#                 ['sleeping', 'drinking', 'eating', 'eating', 'sleeping and eating', 'showering', 'drinking'],
-#                 ['eating', 'drinking', 'sleeping', 'drinking', 'drinking and sleeping', 'eating']]
+#con_activities = [['eating', 'drinking', 'sleeping', 'drinking', 
+#                     'drinking and sleeping', 'showering', 'eating'],
+#                   ['drinking', 'eating', 'drinking and eating', 'sleeping', 
+#                     'doing nothing', 'eating', 'drinking and sleeping'],
+#                   ['sleeping', 'drinking', 'eating', 'eating', 
+#                     'sleeping and eating', 'showering', 'drinking'],
+#                   ['eating', 'drinking', 'sleeping', 
+#                     'drinking', 'drinking and sleeping', 'eating']]
 
 #test = LevenshteinDistance(con_activities).get_levenshtein_distances(con_activities)
 #print(test)
@@ -98,7 +122,8 @@ class EuclideanDistance:
     def __init__(self, list):
         self.list = list
 
-    # Return: Euclidean distances between all pairs of vectors in two lists of strings
+    # Return: Euclidean distances between all pairs of vectors 
+    # in two lists of strings
     # Input: vectors = list of vectors
     def get_euclidean_distances(self, vectors: list) -> list:
         distance = DistanceMetric.get_metric('euclidean')
@@ -126,10 +151,12 @@ class BooleanDistance:
     def get_boolean_distances(self, list: list) -> list:
         distances = []
         # First pointer, outer loop: traverse each word in the list once
-        # The number of inner lists within distances = the number of strings in the first iteration
+        # The number of inner lists within distances is equal 
+        # to the number of strings in the first iteration
         for firstStrings in list:
             sub_distances = []
-            # Second pointer, inner loop: traverse each word for each time it is traversed in the outer loop
+            # Second pointer, inner loop: traverse each word 
+            # for each time it is traversed in the outer loop
             # The number of output booleans = the number of strings in the second iteration
             for secondStrings in list:
                 # If both strings are equivalent, add 1 to the inner list
@@ -139,15 +166,12 @@ class BooleanDistance:
                 else:
                     sub_distances.append(0)
             distances.append(sub_distances)
-        # This part is to normalise distance matrices with non-zero diagonals into zero diagonals
+        # This part is to normalise distance matrices 
+        # with non-zero diagonals into zero diagonals
         for i in range(0, len(distances)):
             distances[i][i] = 0
         return distances
 
-#con_activities = [['eating', 'drinking', 'sleeping', 'drinking', 'drinking and sleeping', 'showering', 'eating'],
-#                 ['drinking', 'eating', 'drinking and eating', 'sleeping', 'doing nothing', 'eating', 'drinking and sleeping'],
-#                 ['sleeping', 'drinking', 'eating', 'eating', 'sleeping and eating', 'showering', 'drinking'],
-#                 ['eating', 'drinking', 'sleeping', 'drinking', 'drinking and sleeping', 'eating']]
 #bool_activites = ['hi', 'hi', 'hello', 'hello', 'hello', 'hi']
 #test = BooleanDistance(bool_activites).get_boolean_distances(bool_activites)
 #print(test)
