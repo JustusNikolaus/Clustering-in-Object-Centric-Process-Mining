6b537e89d9589c3c23749550bfbd0c5457b5b418
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2021-11-18 22:27:17 +0100

    Update tests.py
    Added agglomerative tests and updated the output so that it's easier to read and understand.

--- drawpage/tests.py
+++ drawpage/tests.py
@@ -1,11 +1,12 @@
 #from django.test import TestCase
 from kmedoids import cluster_kmedoids
+from agglomerative import cluster_agglomerative
 from distance_techniques import LevenshteinDistance, BooleanDistance, EuclideanDistance
 import numpy as np
 
 # Create your tests here.
 
-# Test k_medoids
+
 lev_activities = [['eat', 'sleep', 'rave', 'repeat'],
                    ['eat', 'sleep', 'rave', 'Not repeat'],
                    ['eat', 'sleep', 'Not rave', 'Not repeat'],
@@ -19,12 +20,22 @@ bool_activities = ['Not eat', 'Not sleep', 'Not rave', 'Not repeat', 'eat',
 
 euc_activities = np.random.randint(20, size=(3,35))
 
+
 test_lev = LevenshteinDistance(lev_activities).get_levenshtein_distances(lev_activities)
+print("Calculated Levenshtein distances:\n", test_lev, "\n-----------")
 test_bool = BooleanDistance(bool_activities).get_boolean_distances(bool_activities)
+print("Calculated Boolean distances:\n", test_bool, "\n-----------")
 test_euc = EuclideanDistance(euc_activities).get_euclidean_distances(euc_activities)
-#print(test_lev, "\n")
-print(cluster_kmedoids(distance_matrix=test_lev), "\n-----------")
-#print(test_bool, "\n")
-print(cluster_kmedoids(distance_matrix=test_bool), "\n-----------")
-#print(test_euc, "\n")
-print(cluster_kmedoids(distance_matrix=test_euc), "\n-----------")
+print("Calculated Euclidean distances:\n", test_euc, "\n-----------")
+
+# Test k_medoids
+
+print("Calculated clusters for k-medoids clustering with Levenshtein distances:\n", cluster_kmedoids(distance_matrix=test_lev), "\n-----------")
+print("Calculated clusters for k-medoids clustering with Boolean distances:\n", cluster_kmedoids(distance_matrix=test_bool), "\n-----------")
+print("Calculated clusters for k-medoids clustering with Euclidean distances:\n", cluster_kmedoids(distance_matrix=test_euc), "\n-----------")
+
+# Test agglomerative
+
+print("Calculated clusters for hierarchical clustering with Levenshtein distances:\n", cluster_agglomerative(distance_matrix=test_lev), "\n-----------")
+print("Calculated clusters for hierarchical clustering with Boolean distances:\n", cluster_agglomerative(distance_matrix=test_bool), "\n-----------")
+print("Calculated clusters for hierarchical clustering with Euclidean distances:\n", cluster_agglomerative(distance_matrix=test_euc), "\n-----------")
