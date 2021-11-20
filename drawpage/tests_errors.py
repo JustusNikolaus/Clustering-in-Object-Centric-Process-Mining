e8968ffa96f43a63d648f5541a8340d34bb60849
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2021-11-20 22:00:29 +0100

    Small fixes to error handling in distance_techniques.py
    Fixed the error handling so that it actually raises the exceptions instead of defaulting to the regular exception. Renamed some of the testing codes so that they are easier to find.

--- drawpage/tests_errors.py
+++ drawpage/tests_errors.py
@@ -7,14 +7,14 @@ def map_labels_to_activities(labels: list, activities: list) -> list:
             if not isinstance(labels, list) or not isinstance(activities, list):
                 raise TypeError # raise an error if one of the inputs is not a list
             if len(labels) != len(activities):
-                raise UnequalListsError # raise an error if they are not equal in length
+                raise UnequalListsError(labels, "Error") # raise an error if they are not equal in length
             for given_label, labelled_activity in zip(labels, activities): # for each given label
                 if not isinstance(given_label, str) or not isinstance(labelled_activity, str):
                     raise TypeError # raise an error if one of the values within the passed lists is not a string
-                if given_label in map.keys(): # if the key is not already in the map
-                    raise DuplicatesError # raise an error if they are not unique
-                elif given_label not in map.keys():
+                if given_label not in map.keys():
                     map[given_label] = labelled_activity # create key and add the corresponding activity
+                else: # if the key is already in the map, then it is a duplicate
+                    raise DuplicatesError(given_label, "Error") # raise an error if they are not unique
         except UnequalListsError:
             print("The labels and activities do not map one-to-one")
         except DuplicatesError:
@@ -31,4 +31,6 @@ labels_not = 'a'
 activities = ['hi', 'bye', 'hello']
 
 print(map_labels_to_activities(labels_correct, activities))
+print(map_labels_to_activities(labels_unequal, activities))
+print(map_labels_to_activities(labels_not, activities))
 print(map_labels_to_activities(labels_duplicate, activities))
