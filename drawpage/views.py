bc919efd77e69d0ec4538e87f0f723f1d7a6b795
simonmichau <57032947+simonmichau@users.noreply.github.com>
2021-11-23 11:25:41 +0100

    Improved session handling

--- drawpage/views.py
+++ drawpage/views.py
@@ -19,34 +19,19 @@ def drawpage_view(request):
         # If file_select_form
         if 'file_select' in request.POST:
             selected_file = request.POST['file_select']
-            # Create object_type_list
-            ocel_object_dict_list = readocel.get_object_types('media/' + selected_file)
-            #print(ocel_object_dict_list)
-            for i in range(len(ocel_object_dict_list)):
-                object_type_list.append(ocel_object_dict_list[i].get('object_type'))
-            # Clear old object_type_cookie and object_cookie
+
+            # Clear old object_type_cookie
             if 'object_type_cookie' in request.session:
                 del request.session['object_type_cookie']
 
-            # Save file_cookie and write to top of file_list
+            # Save file_cookie
             request.session['file_cookie'] = selected_file
-            file_list.insert(0, request.session['file_cookie'])
         # If object_type_select_form
         elif 'object_type_select' in request.POST:
             selected_object_type = request.POST['object_type_select']
 
-            if 'file_cookie' in request.session:
-                # Create object_type_list
-                ocel_object_dict_list = readocel.get_object_types('media/' + request.session['file_cookie'])
-                for i in ocel_object_dict_list:
-                    object_type_list.append(i.get('object_type'))
-
-                # Keep selected file in top of file_list
-                file_list.insert(0, request.session['file_cookie'])
-
             # Save object_type_cookie
             request.session['object_type_cookie'] = selected_object_type
-            object_type_list.insert(0, request.session['object_type_cookie'])
         # If clustering_method_select and event_assignment_select
         elif 'clustering_method_select' in request.POST and 'event_assignment_select' in request.POST:
             selected_clustering_method = request.POST['clustering_method_select']
@@ -67,20 +52,14 @@ def drawpage_view(request):
             # Save event_assignment_cookie
             request.session['event_assignment_cookie'] = selected_event_assignment
 
-            if 'file_cookie' in request.session:
-                # Create object_type_list
-                if 'object_type_cookie' in request.session and 'file_cookie' in request.session:
-                    ocel_object_dict_list = readocel.get_object_types('media/' + request.session['file_cookie'])
-                    for i in ocel_object_dict_list:
-                        object_type_list.append(i.get('object_type'))
-
+            if 'file_cookie' in request.session:                 
                 # Call main_draw
-                if 'file_cookie' in request.session and 'object_type_cookie' in request.session and 'clustering_method_cookie' in request.session:
+                if 'object_type_cookie' in request.session:
                     path_to_file = 'media/' + request.session['file_cookie']
                     print("Path to file is: {}".format(path_to_file))
                     object_information = readocel.get_object_types(path_to_file)
                     print("Object information is: {}".format(object_information))
-                    dfg_file_path_list = main.main_draw(path_to_file, object_information, request.session['object_type_cookie'], request.session['clustering_method_cookie'], selected_event_assignment)
+                    dfg_file_path_list = main.main_draw(path_to_file, object_information, request.session['object_type_cookie'], selected_clustering_method, selected_event_assignment)
                     # Remove leading '.' from file paths
                     dfg_file_path_list = [sub[1 : ] for sub in dfg_file_path_list]
 
@@ -91,6 +70,15 @@ def drawpage_view(request):
             file_list.append(files)
         else:
             continue
+    if 'file_cookie' in request.session:
+        file_list.insert(0, request.session['file_cookie'])
+
+        # Refresh object_type_list
+        ocel_object_dict_list = readocel.get_object_types('media/' + request.session['file_cookie'])
+        for i in ocel_object_dict_list:
+            object_type_list.append(i.get('object_type'))
+        if 'object_type_cookie' in request.session:
+            object_type_list.insert(0, request.session['object_type_cookie'])
 
     # Keep clustering_method
     if 'clustering_method_cookie' in request.session:
@@ -106,7 +94,7 @@ def drawpage_view(request):
         elif request.session['event_assignment_cookie'] == "existence":
             event_assignment = ['', 'checked']
 
-    # For Debugging: print all cookies
+    # FOR DEBUGGING: print all cookies
     if 'file_cookie' in request.session:
         print("----->File: " + request.session['file_cookie'])
     if 'object_type_cookie' in request.session:
