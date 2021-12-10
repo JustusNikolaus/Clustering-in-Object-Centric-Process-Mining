8bd44c1d4ac70db8d20afd8b36bc45dd0b1220dd
Ventsislava Kondakova <ventsislavakondakova@Ventsislavas-MacBook-Pro.local>
2021-12-10 03:43:53 +0100

    Added delete_file function *not working*

--- importpage/views.py
+++ importpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from os import walk
+from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
+from os import listdir
+from os.path import isfile, join
 
 import os
  
@@ -14,6 +18,7 @@ def importpage_view(request):
 def upload_log(request):
     response_label = []
     file_list = []
+    event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
 
 
     if request.method == 'POST':
@@ -31,16 +36,26 @@ def upload_log(request):
                 response_label = ['danger','Please select a file first']
                 
         #TODO: delete function
-        # elif 'delete_log' in request.POST:
-        #     try:
-        #         print("test")
-        #         os.remove(request.POST['file'])
-        #     except: 
-        #         response_label = ['danger','Please select a file to delete']
+        elif 'delete_log' in request.POST:
+            try:
+                if "log_list" not in request.POST:
+                    return HttpResponseRedirect(request.path_info)
+        
+                filename = request.POST["file_list"]
+
+                eventlogs = [f for f in listdir(event_logs_path) if isfile(join(event_logs_path, f))]
+                
+                eventlogs.remove(filename)
+                file_dir = os.path.join(event_logs_path, filename)
+                os.remove(file_dir)
+                
+                response_label = ['success','Successful delete']
+            except: 
+                response_label = ['danger','Please select a file to delete']
     
     ext = ('.xmlocel','.jsonocel')
     for files in os.listdir('media/'):
         if files.endswith(ext):
             file_list.append(files)
 
-    return render(request, 'importpage/importpage.html', {'response_label':response_label, 'file_list': file_list})
+    return render(request, 'importpage/importpage.html', {'response_label':response_label, 'file_list': file_list, 'eventlog_list': eventlogs})
